b7decbd7f8c7754fa54d2477aaf96125213a8660
Berke Kanber <berkekanber@gmail.com>
2024-09-08 14:14:27 +0300

    feat: plans layout

--- src/components/ui/Premium/Plans.tsx
+++ src/components/ui/Premium/Plans.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-} from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody } from "@nextui-org/react";
 import { BsStars } from "react-icons/bs";
 
 function Plans() {
@@ -27,86 +21,60 @@ function Plans() {
       <Modal isOpen={isOpen} onOpenChange={setIsOpen} size="5xl">
         <ModalContent>
           <>
-            <ModalHeader className="flex flex-col gap-1 text-center">
+            <ModalHeader className="flex flex-col gap-1 text-center mt-8">
               <h1 className="text-3xl">Select Plans</h1>
-              <p className="text-sm text-gray-400 font-normal ml-16 mr-16">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Magnam, a iure necessitatibus et ipsum vitae, maxime eos facere,
-                est perferendis optio cum vero dignissimos eligendi distinctio
-                labore architecto commodi veniam.
+              <p className="text-[15px] text-gray-400 font-normal mx-48">
+                Choose the plan that fits your needs and start your journey today. Enjoy flexibility, great features, and support tailored to help you succeed.
               </p>
             </ModalHeader>
             <ModalBody className="mb-4">
               <div className="flex justify-center space-x-8">
-                <div className="bg-white shadow-md border border-gray-300 rounded-lg p-6 w-64 text-center flex flex-col justify-between">
+                <div className="bg-white shadow-md border border-gray-300 rounded-lg p-8 w-72 my-12 py-16 text-center flex flex-col justify-between">
                   <div>
-                    <h3 className="text-2xl font-semibold mb-4 text-gray-700">
-                      Personal
-                    </h3>
-                    <p className="text-xl font-bold text-blue-500">Free</p>
-                    <p className="text-gray-500 mb-4">
-                      For people hosting small, ad hoc social gatherings.
-                    </p>
+                    <h3 className="text-2xl font-semibold mb-4 text-gray-700">Personal</h3>
+                    <p className="text-xl font-bold text-blue-500 mb-4">Free</p>
+                    <p className="text-gray-500 mb-4">Ideal for managing tasks and projects with ease.</p>
                     <ul className="text-left mb-4">
-                      <li>✔️ Up to 10 participants at once</li>
-                      <li>✔️ Unlimited events</li>
-                      <li>✔️ Unlimited organizers</li>
-                      <li>✔️ Standard and advanced matching</li>
+                      <li>✔️ Create unlimited boards and tasks</li>
+                      <li>✔️ Collaborate with your team</li>
+                      <li>✔️ Task prioritization and tracking</li>
+                      <li>✔️ Real-time updates and notifications</li>
                     </ul>
                   </div>
-                  <button className="w-full bg-blue-500 text-white py-5 rounded-lg mt-auto">
-                    Get Started for Free
-                  </button>
+                  <button className="w-full bg-blue-500 text-white py-3 rounded-lg mt-auto hover:opacity-80">Start Using for Free</button>
                 </div>
-                <div className="bg-blue-500 text-white shadow-md rounded-lg p-6 w-64 text-center flex flex-col justify-between">
+
+                <div className="bg-blue-500 text-white shadow-md rounded-lg w-72 p-8 my-12 py-16 text-center flex flex-col justify-between">
                   <div>
                     <h3 className="text-2xl font-semibold mb-4">Premium</h3>
-                    <p className="text-xl font-bold">$5</p>
-                    <p className="mb-4">
-                      per participant, per month. For Company
-                    </p>
+                    <p className="text-xl font-bold mb-4">$5</p>
+                    <p className="mb-4">per user, per month. Ideal for businesses and teams.</p>
                     <ul className="text-left mb-4">
-                      <li>✔️ Everything in Personal</li>
-                      <li>✔️ Up to 200 participants at once</li>
-                      <li>✔️ Unlimited events</li>
-                      <li>✔️ Event data exports</li>
+                      <li>✔️ Everything in Free Plan</li>
+                      <li>✔️ Manage up to 200 tasks at once</li>
+                      <li>✔️ Priority support and advanced features</li>
+                      <li>✔️ Task and project data exports</li>
                     </ul>
                   </div>
-                  <button className="w-full bg-white text-blue-500 py-2 rounded-lg mt-auto">
-                    Get Started with
-                    <br /> Premium
-                  </button>
+                  <button className="w-full bg-white text-blue-500 py-3 rounded-lg mt-auto font-medium hover:opacity-80">Started with Premium</button>
                 </div>
-                <div className="bg-gray-800 text-white shadow-md rounded-lg p-6 w-64 text-center flex flex-col justify-between">
+
+                <div className="bg-gray-800 text-white shadow-md rounded-lg p-8 w-72 my-12 py-16 text-center flex flex-col justify-between">
                   <div>
                     <h3 className="text-2xl font-semibold mb-4">Business</h3>
-                    <p className="text-xl font-bold">$15</p>
-                    <p className="mb-4">
-                      per participant, per month. For enterprises
-                    </p>
+                    <p className="text-xl font-bold mb-4">$15</p>
+                    <p className="mb-4">per user, per month. Best for large enterprises.</p>
                     <ul className="text-left mb-4">
-                      <li>✔️ Everything in Premium</li>
-                      <li>✔️ Up to 500 participants at once</li>
-                      <li>✔️ Dedicated support</li>
-                      <li>✔️ Custom reporting and analytics</li>
-                      <li>✔️ Enterprise-level security</li>
+                      <li>✔️ Everything in Premium Plan</li>
+                      <li>✔️ Manage up to 500 tasks at once</li>
+                      <li>✔️ Advanced reporting and custom analytics</li>
+                      <li>✔️ Enterprise-level security and compliance</li>
                     </ul>
                   </div>
-                  <button className="w-full bg-white text-gray-800 py-2 rounded-lg mt-auto">
-                    Get Started with
-                    <br /> Business
-                  </button>
+                  <button className="w-full bg-white text-gray-800 py-3 rounded-lg mt-auto font-medium hover:opacity-80">Started with Business</button>
                 </div>
               </div>
             </ModalBody>
-            <ModalFooter className="justify-center">
-              <button
-                className="bg-red-400 text-white px-6 py-2 rounded-lg hover:bg-red-500"
-                onClick={closeModal}
-              >
-                Close
-              </button>
-            </ModalFooter>
           </>
         </ModalContent>
       </Modal>
