01a97ef7bf5aaca51bd81aad990e944ca090c864
Berke Kanber <berkekanber@gmail.com>
2024-09-14 20:14:39 +0300

    refactor: soon page design

--- src/app/soon/page.tsx
+++ src/app/soon/page.tsx
@@ -1,25 +1,27 @@
+import Image from "next/image";
 import React from "react";
 
-import thumbs from "../../../public/thumbs up.png";
 import logo from "../../../public/logo.svg";
-import Image from "next/image";
+import thumbs from "../../../public/thumbs up.png";
 
-function page() {
+function Page() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <div className="absolute w-64 h-64 bg-[#4B4EE7] blur-[200px] top-1/2 left-3/4"></div>
-      <div className="absolute w-64 h-64 bg-[#8276FF] blur-[200px] top-1/3 right-3/4"></div>
-      <div className="pt-72 flex flex-col items-center justify-center w-full h-full relative z-0">
-        <div className="mt-2">
-          <Image src={logo} width={120} height={120} alt="lumilogo" />
+    <>
+      <div className="w-64 h-64 bg-[#4B4EE7] blur-[200px] absolute top-1/4 left-3/4 z-0"></div>
+      <div className="w-64 h-64 bg-[#4B4EE7] blur-[200px] absolute top-1/2 right-3/4 z-0"></div>
+      <div className="flex flex-col justify-between w-full min-h-screen relative z-10">
+        <div className="flex items-center justify-center flex-grow">
+          <div className="flex flex-col items-center justify-center">
+            <Image src={logo} width={130} height={130} alt="logo" />
+            <div className="font-black text-[64px] pt-8 text-[#4B4EE7]">Cooking Our Website.</div>
+            <div className="text-gray-500 font-medium pt-3">We are going to launch our website Very Soon.</div>
+            <div className="text-gray-500 font-medium pt-2">Stay Tune.</div>
+            <div className="mt-7">
+              <Image src={thumbs} width={50} height={50} alt="thumbs" />
+            </div>
+          </div>
         </div>
-        <div className="font-extrabold text-[64px] max-sm:text-[48px] text-[#4B4EE7] my-4 mt-10 text-center">Cooking Our Website.</div>
-        <div className="text-gray-500 font-medium">We are going to launch our website Very Soon.</div>
-        <div className="mt-2 text-gray-500 font-medium">Stay Tune.</div>
-        <div className="mt-7">
-          <Image src={thumbs} width={50} height={50} alt="thumbs" />
-        </div>
-        <div className="flex items-end h-full mb-5">
+        <div className="flex justify-center py-4">
           <h2 className="text-gray-500 font-medium">
             Powered by{" "}
             <a target="_blank" href="https://github.com/lumi-work" className="text-gray-600 underline decoration-wavy decoration-[#4B4EE7]">
@@ -28,8 +30,8 @@ function page() {
           </h2>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
-export default page;
+export default Page;
